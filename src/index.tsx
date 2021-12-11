3ad520e9566f56876e3951a83249dda48bc84dc2
Zislin Vasiliy <zislin.maestro@yandex.ru>
2021-12-11 11:00:47 +0300

    new commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
+// const element = <h1 className="test">Hello World</h1>;
+
+// const elementTwo = React.createElement(
+//   "h1",
+//   { className: "test" },
+//   "Привет Мир"
+// );
+
+// ReactDOM.render(elementTwo, document.getElementById("root"));
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
